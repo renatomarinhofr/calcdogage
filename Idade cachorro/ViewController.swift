045cccd86f90132ac4cf122f8a736e8a858bc4d3
Renato Marinho <renato.marinhofr@gmail.com>
2022-02-19 01:14:02 -0300

    first app swift

--- Idade cachorro/ViewController.swift
+++ Idade cachorro/ViewController.swift
@@ -8,7 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var textResult: UILabel!
+    
+    @IBOutlet weak var fieldAge: UITextField!
+    
+    @IBAction func calcAge(_ sender: Any) {
+        
+        let idade = Int(fieldAge.text!)! * 7
+        
+         textResult.text = "A idade humana do cachorro é " + String(idade)
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
